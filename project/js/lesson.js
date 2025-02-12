b9919c65cef676420dae17b69f574fe110e3e376
Nurzada <139949446+Nurzadaprojects@users.noreply.github.com>
2025-02-12 12:48:14 +0600

    Add files via upload

--- project/js/lesson.js
+++ project/js/lesson.js
@@ -13,3 +13,54 @@ phoneButton.onclick = () => {
         phoneResult.style.color = 'red'
     }
 }
+
+//TAB SLIDER homework 3 
+
+const tabContentBlocks = document.querySelectorAll('.tab_content_block')
+const tabItems = document.querySelectorAll('.tab_content_item')
+const tabsParent = document.querySelector('.tab_content_items')
+let currentIndex = 0;
+let autoSwitch;
+
+const hideTabContent = () => {
+    tabContentBlocks.forEach((item) => {
+        item.style.display = 'none'
+    })
+    tabItems.forEach((item) => {
+        item.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabContent = (index = 0) => {
+    tabContentBlocks[index].style.display = 'block'
+    tabItems[index].classList.add('tab_content_item_active')
+}
+
+const startAutoSwitch = () => {
+    autoSwitch = setInterval(() => {
+        currentIndex = (currentIndex + 1) % tabContentBlocks.length
+        hideTabContent()
+        showTabContent(currentIndex)
+    }, 4000)
+}
+
+hideTabContent()
+showTabContent()
+startAutoSwitch()
+
+let timeout
+
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabItems.forEach((item, i) => {
+            if (event.target === item) {
+                clearInterval(autoSwitch)
+                clearTimeout(timeout)
+                hideTabContent()
+                showTabContent(i)
+                currentIndex = i
+                timeout = setTimeout(startAutoSwitch, 1000)
+            }
+        })
+    }
+}
